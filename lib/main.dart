93db28357abc734dac833e5cc7e99f6093dbdebd
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2025-03-20 10:49:42 -0300

    feat: removido ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:http/http.dart' as http;
-import 'package:open_gate/banner_adb_mob.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -34,24 +31,17 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: SafeArea(
           child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const BannerAdbMob(),
-              IconButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.indigo,
-                  shape: const CircleBorder(),
-                ),
-                onPressed: toogleActionOfGate,
-                icon: const Icon(
-                  Icons.power_settings_new,
-                  color: Colors.white,
-                  size: 200,
-                ),
-              ),
-              const BannerAdbMob()
-            ],
+              child: IconButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigo,
+              shape: const CircleBorder(),
+            ),
+            onPressed: toogleActionOfGate,
+            icon: const Icon(
+              Icons.power_settings_new,
+              color: Colors.white,
+              size: 200,
+            ),
           )),
         ));
   }
